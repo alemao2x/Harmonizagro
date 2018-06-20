0389befaf6bcdfddbeee294862953b60eb7feb30
Gian <giancarlo.tutini@hotmail.com>
2018-06-20 03:45:10 -0300

    Desgraça

---
+++ app/src/main/java/com/example/gian/harmonizagro/DisplayActivity.java
@@ -0,0 +1,20 @@
+package com.example.gian.harmonizagro;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
